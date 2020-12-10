9210105b8e0901db2aa8f87c04f4a0944c4c7b58
Michael Zhang <mzhang1628@gmail.com>
2020-12-09 22:29:36 -0800

    added turtlebot with and w/o camera sensing

--- script/turtlebot_sensor.py
+++ script/turtlebot_sensor.py
@@ -3,6 +3,9 @@ import pybullet_data
 import time
 from ball import *
 from TurtleSFOC import *
+import cv2
+import numpy as np
+import KalmanFilter
 
 bot_height = 0.4
 ball_radius = 0.1
@@ -32,20 +35,50 @@ def get_ball_position(ball):
     '''
     ball_p_o = p.getBasePositionAndOrientation(ball)
     ball_position = ball_p_o[0]  # x y z coordinate of the ball
-    ball_x = ball_position[0]
-    ball_y = ball_position[1]
-    ball_z = ball_position[2]
-    return ball_x, ball_y, ball_z
+    return ball_position
 
 
 def get_turtle_velocity(turtle):
-    '''
-Get the velocity of turtlebot's right and left wheels.
+    '''                      
+    Get the velocity of turtlebot's right and left wheels.
     '''
     turtle_left = p.getJointState(turtle, 0)[1]
     turtle_right = p.getJointState(turtle, 1)[1]
     turtle_pre = [turtle_right, turtle_left]
     return turtle_pre
+
+
+def get_coord_from_cam(images):
+    rgb_opengl = np.reshape(images[2], (height, width, 4)) * 1. / 255.
+    depth_buffer_opengl = np.reshape(images[3], [width, height])
+    depth_opengl = far * near / (far - (far - near) * depth_buffer_opengl)
+    seg_opengl = np.reshape(images[4], [width, height]) * 1. / 255.
+    img_coords = get_ball_coord(rgb_opengl[:, :, :3].astype(np.float32))
+    est_coord = depth_opengl[img_coords[0], img_coords[1]] - 5
+    return est_coord
+
+
+def get_z_from_cam(images):
+    rgb_opengl = np.reshape(images[2], (height, width, 4)) * 1. / 255.
+    depth_buffer_opengl = np.reshape(images[3], [width, height])
+    depth_opengl = far * near / (far - (far - near) * depth_buffer_opengl)
+    seg_opengl = np.reshape(images[4], [width, height]) * 1. / 255.
+    img_coords = get_ball_coord(rgb_opengl[:, :, :3].astype(np.float32))
+    print(f'img_coords: {img_coords}')
+    est_coord = 5 - depth_opengl[img_coords[0], img_coords[1]]
+    return est_coord
+
+
+def get_ball_coord(rgb_img):
+    output_frame = rgb_img.copy()
+    captured_frame_red = cv2.inRange(
+        rgb_img*255, np.array([40, 0, 0]), np.array([255, 100, 100]))
+    coords = np.mean(np.transpose(np.nonzero(captured_frame_red)), axis=0)
+    cv2.imshow('frame', captured_frame_red)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # cv2.waitKey(1)
+    return coords.astype(np.int32)
 
 
 p.connect(p.GUI)
@@ -54,8 +87,34 @@ offset = [0, 0, 0]
 turtle = p.loadURDF("../model/turtlebot.urdf", offset, globalScaling=1.0)
 plane = p.loadURDF("../model/plane.urdf")
 # p.setRealTimeSimulation(1)
+
+width = 128
+height = 128
+
+fov = 60
+aspect = width / height
+near = 0.02
+far = 10
+
+dt = simulation_step
+F = np.array([[1, dt, 0], [0, 1, dt], [0, 0, 1]])
+H = np.array([1, 0, 0]).reshape(1, 3)
+Q = np.array([[0.05, 0.05, 0.0], [0.05, 0.05, 0.0], [0.0, 0.0, 0.0]])
+R = np.array([0.5]).reshape(1, 1)
+kf = KalmanFilter.KalmanFilter(F = F, H = H, Q = Q, R = R)
+
+x = np.linspace(-10, 10, 100)
+measurements = - (x**2 + 2*x - 2) + np.random.normal(0, 2, 100)
+
+view_matrix_x = p.computeViewMatrix([-5, 0, 2.5], [0, 0, 2.5], [0, 0, 1])
+view_matrix_y = p.computeViewMatrix([0, -5, 2.5], [0, 0, 2.5], [0, 0, 1])
+view_matrix_z = p.computeViewMatrix([2.5, 2.5, 5], [2.5, 2.5, 0], [1, 0, 0])
+
+projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, near, far)
+
 # create elastic ball
-elastic_ball = create_elastic_ball(2, 1.5, 1)
+elastic_ball = create_elastic_ball(2, 2, 1)
+# elastic_ball = create_elastic_ball(1, 3, 1)
 # elastic collision with the floor plane
 p.changeDynamics(plane, -1,
                  #  lateralFriction=0,
@@ -67,12 +126,14 @@ p.changeDynamics(plane, -1,
 
 p.setGravity(0, 0, global_g)
 
+prev_x, prev_y, prev_z = 0, 0, 0
+
 for j in range(p.getNumJoints(turtle)):
     print(p.getJointInfo(turtle, j))
 forward = 0
 turn = 0
 force_applied = False
-# logId1 = p.startStateLogging(p.STATE_LOGGING_GENERIC_ROBOT, "LOG0003.txt")
+# logId1 = p.startStateLogging(p.STATE_LOGGING_GENERIC_ROBOT, "forward_sensor.txt")
 while p.isConnected():
     print('-------------')
     # p.setRealTimeSimulation(1)
@@ -80,19 +141,72 @@ while p.isConnected():
     # NEED TO USE STEP SIMULATION
     p.stepSimulation()
     p.setTimeStep(simulation_step)
+
     if not force_applied:
         p.applyExternalForce(
-            elastic_ball, -1, [50, 50, 0], p.getBasePositionAndOrientation(elastic_ball)[0], flags=p.WORLD_FRAME)
+            elastic_ball, -1, [15, 7.5, 0], p.getBasePositionAndOrientation(elastic_ball)[0], flags=p.WORLD_FRAME)
         force_applied = True
+
+    ball_x, ball_y, ball_z = get_ball_position(elastic_ball)
+
+    # Get depth values using the OpenGL renderer
+    images_x = p.getCameraImage(width,
+                                height,
+                                view_matrix_x,
+                                projection_matrix,
+                                shadow=True,
+                                renderer=p.ER_BULLET_HARDWARE_OPENGL)
+    images_y = p.getCameraImage(width,
+                                height,
+                                view_matrix_y,
+                                projection_matrix,
+                                shadow=True,
+                                renderer=p.ER_BULLET_HARDWARE_OPENGL)
+    images_z = p.getCameraImage(width,
+                                height,
+                                view_matrix_z,
+                                projection_matrix,
+                                shadow=True,
+                                renderer=p.ER_BULLET_HARDWARE_OPENGL)
+
+    # get estimated ball x coordinate
+    estimate_x = get_coord_from_cam(images_x)
+    estimate_y = get_coord_from_cam(images_y)
+    estimate_z = get_z_from_cam(images_z)
+
+    # dummy_vx = (estimate_x - prev_x) / 0.02
+    # prev_x = dummy_vx
+
+    estimate_vx = np.dot(H,  kf.predict())[0]
+    kf.update(estimate_x)
+
+    # estimate_yv = (estimate_y - prev_y) / 0.02
+    # prev_y = estimate_y
+
+    print(f'True ball X: {ball_x}')
+    print(f'Estimated ball X: {estimate_x}')
+
+    print(f'True ball Y: {ball_y}')
+    print(f'Estimated ball Y: {estimate_y}')
+
+    print(f'True ball Z: {ball_z}')
+    print(f'Estimated ball Z: {estimate_z}')
+    # print(est_x, ball_x)
 
     start_time = time.time()
     # ----- compute controls -----
     turtle_z0 = turtle_get_z0(turtle)
     # get ball states
-    ball_x, ball_y, ball_z = get_ball_position(elastic_ball)
+    # ball_x, ball_y, ball_z = get_ball_position(elastic_ball)
+    ball_x, ball_y, ball_z = estimate_x, estimate_y, estimate_z
     ball_z = ball_z - ball_radius
 
     ball_vx, ball_vy, ball_vz = p.getBaseVelocity(elastic_ball)[0]
+
+    print(f'Ball X Velocity: {ball_vx}')
+    print(f'Estimate X Velocity: {estimate_vx}')
+    # print(f'Ball Y Velocity: {ball_vy}')
+    # print(f'Estimate Y Velocity: {estimate_yv}')
 
     turtle_pre = get_turtle_velocity(turtle)
     Time_limit = 2
@@ -160,8 +274,8 @@ while p.isConnected():
         print(f'T_lower before: {T_lower}')
         print(f'T_upper before: {T_upper}')
         # feed current info into the feedback control algorithm
-        while T_upper < guess_period and turtle_velocity > 0.2:
-            T_upper = T_upper + T_period
+        # while T_upper < guess_period and turtle_velocity > 0.2:
+        #     T_upper = T_upper + T_period
         print(f'T_lower after: {T_lower}')
         print(f'T_upper after : {T_upper}')
         if T_upper > 0.5:
