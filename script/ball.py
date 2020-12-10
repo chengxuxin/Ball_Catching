3c707d8094cf11c3ae457bd49c959a29dd66b0d7
Michael Zhang <mzhang1628@gmail.com>
2020-12-09 22:26:27 -0800

    modified ball weight

--- script/ball.py
+++ script/ball.py
@@ -13,10 +13,10 @@ def create_elastic_ball(ball_x, ball_y, ball_z):
                                          specularColor=[0.4, .4, 0])
 
     ball_collision_id = p.createCollisionShape(shapeType=p.GEOM_SPHERE,
-                                               radius=0.15)
+                                               radius=0.1)
 
     # create the ball with visual, collision, and initial position
-    ball_id = p.createMultiBody(baseMass=1,
+    ball_id = p.createMultiBody(baseMass=0.5,
                                 baseCollisionShapeIndex=ball_collision_id,
                                 baseVisualShapeIndex=ball_visual_id,
                                 basePosition=ball_initial_position)
